b5e39ce0a2469837ad045c622f58ad8d7b4cd90e
Muhammed Habeeb Rahman KT <muhammadhabeebrahmam@gmail.com>
2025-11-14 22:34:51 +0530

    Delete controller directory

--- controller/booking/helper/calculateNights.js
+++
@@ -1,9 +0,0 @@
-export const calculateNights = (checkIn, checkOut) => {
-    const check_out = new Date(checkOut);
-    const check_in = new Date(checkIn);
-
-    const timeDiff = check_out - check_in;
-    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return daysDiff;
-};
-
